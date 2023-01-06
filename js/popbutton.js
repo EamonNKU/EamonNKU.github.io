6d1cdf83ab128ce2d681dea198937b9adbc033f5
Eamon's Lingyao <572464453@qq.com>
2023-01-07 03:47:55 +0800

    Site updated: 2023-01-07 03:47:54

--- js/popbutton.js
+++ js/popbutton.js
@@ -1,15 +1,1 @@
-window.onload=function(){
-	var div = document.getElementById('background-tcpop');
-	var tcclose = document.getElementById("close-tcbutton");
-	tcshow=function(){  
-		div.style.display = "block";
-        	}
-          tcclose.onclick = function tcclose() {
-          	div.style.display = "none";
-          }
-          window.onclick = function tcclose(e) {
-          	if (e.target == div) {
-              		div.style.display = "none";
-           	}
-        	}
-}
+window.onload=function(){var n=document.getElementById("background-tcpop"),o=document.getElementById("close-tcbutton");tcshow=function(){n.style.display="block"},o.onclick=function(){n.style.display="none"},window.onclick=function(o){o.target==n&&(n.style.display="none")}};
