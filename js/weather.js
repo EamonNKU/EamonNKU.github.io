37386405867486714fe58406201f70fb11be2773
EamonNKU <572464453@qq.com>
2023-10-12 06:54:46 +0800

    Site updated: 2023-10-12 06:54:43

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,21 @@
-WIDGET={CONFIG:{modules:"20",background:"5",tmpColor:"FFFF00",tmpSize:"16",cityColor:"CFE2F3",citySize:"16",aqiColor:"B6D7A8",aqiSize:"16",weatherIconSize:"24",alertIconSize:"18",padding:"10px 10px 10px 10px",shadow:"0",language:"auto",fixed:"false",vertical:"top",horizontal:"left",key:"580b1f504e684317be2d354b157bfdab"}};
+WIDGET = {
+  "CONFIG": {
+    "modules": "20",
+    "background": "5",
+    "tmpColor": "FFFF00",
+    "tmpSize": "16",
+    "cityColor": "CFE2F3",
+    "citySize": "16",
+    "aqiColor": "B6D7A8",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "580b1f504e684317be2d354b157bfdab"
+  }
+}
